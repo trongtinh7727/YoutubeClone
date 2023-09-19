1b7fe992bb115003ddac45b76f10bc44ea2e4d3e
trongtinh7727 <trongtinh7727@gmail.com>
2023-09-19 17:36:16 +0700

    feat(control): add toggle event(play, stop)

--- app/src/main/java/com/iiex/videocommunity/activities/ViewActivity.java
+++ app/src/main/java/com/iiex/videocommunity/activities/ViewActivity.java
@@ -5,10 +5,14 @@ import static com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -32,9 +36,11 @@ public class ViewActivity extends AppCompatActivity {
     private ViewViewModel viewModel;
     private VideoView videoView;
     private ActivityViewBinding binding; // DataBinding
-    private GridView gridView;
+    private LinearLayout one,two,three,four;
+    boolean isOpen = true;
 
-    private ImageButton back;
+
+    private ImageButton ibtnBack, ibtnToggle;
 
 
     @Override
@@ -47,16 +53,37 @@ public class ViewActivity extends AppCompatActivity {
         binding.setViewModel(viewModel);
         videoView = binding.videoView;
 
+        // tham chiếu đến custom controls
+        one = findViewById(R.id.videoView_one_layout);
+        two = findViewById(R.id.videoView_three_layout);
+        three = findViewById(R.id.videoView_four_layout);
+        four = findViewById(R.id.video_five_layout);
+        ibtnBack = findViewById(R.id.videoView_go_back);
+        ibtnToggle = findViewById(R.id.videoView_play_pause_btn);
+
         // Lấy dữ liệu từ Intent
         Intent intent = getIntent();
         Video video = (Video) intent.getSerializableExtra("video");
 
         // Đặt dữ liệu vào ViewViewModel
-
         if (video != null) {
             viewModel.setVideo(video, videoView);
             viewModel.togglePlayPause();
         }
+
+        // set sự kiện ẩn control
+        binding.zoomLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpen){
+                    hideDefaultControls();
+                    isOpen = false;
+                }else {
+                    showDefaultControls();
+                    isOpen = true;
+                }
+            }
+        });
 
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -64,7 +91,7 @@ public class ViewActivity extends AppCompatActivity {
         transaction.addToBackStack(null);
         transaction.commit();
 
-        back = findViewById(R.id.videoView_go_back);
+
 
 
 
@@ -82,6 +109,20 @@ public class ViewActivity extends AppCompatActivity {
                 bottomSheetBehavior.setState(STATE_HIDDEN);
             }
         });
+
+        ibtnToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModel.togglePlayPause();
+                if (viewModel.getIsPlaying().getValue()) {
+                    ibtnToggle.setBackgroundResource(R.drawable.ic_pause);
+                }
+                else{
+                    ibtnToggle.setBackgroundResource(R.drawable.ic_play);
+                }
+            }
+        });
+
         setBack();
 
 
@@ -94,12 +135,67 @@ public class ViewActivity extends AppCompatActivity {
 
 
     private void setBack(){
-        back.setOnClickListener(new View.OnClickListener() {
+        ibtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent( getBaseContext(),MainActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void hideDefaultControls(){
+
+        one.setVisibility(View.GONE);
+        two.setVisibility(View.GONE);
+        three.setVisibility(View.GONE);
+        four.setVisibility(View.GONE);
+        final Window window = this.getWindow();
+        if (window == null){
+            return;
+        }
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        final View decorView = window.getDecorView();
+        if (decorView!=null){
+            int uiOption = decorView.getSystemUiVisibility();
+            if (Build.VERSION.SDK_INT >= 14) {
+                uiOption |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+            }
+            if (Build.VERSION.SDK_INT >= 16) {
+                uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            }
+            if (Build.VERSION.SDK_INT >= 19) {
+                uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            }
+            decorView.setSystemUiVisibility(uiOption);
+        }
+    }
+
+    private void showDefaultControls(){
+        one.setVisibility(View.VISIBLE);
+        two.setVisibility(View.VISIBLE);
+        three.setVisibility(View.VISIBLE);
+        four.setVisibility(View.VISIBLE);
+        final Window window = this.getWindow();
+        if (window == null){
+            return;
+        }
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        final View decorView = window.getDecorView();
+        if (decorView!=null){
+            int uiOption = decorView.getSystemUiVisibility();
+            if (Build.VERSION.SDK_INT >= 14) {
+                uiOption &= ~View.SYSTEM_UI_FLAG_LOW_PROFILE;
+            }
+            if (Build.VERSION.SDK_INT >= 16) {
+                uiOption &= ~View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            }
+            if (Build.VERSION.SDK_INT >= 19) {
+                uiOption &= ~View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            }
+            decorView.setSystemUiVisibility(uiOption);
+        }
     }
 }
